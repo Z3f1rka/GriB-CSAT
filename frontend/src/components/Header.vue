e978ad86c21bc7ff416c33457d7fb9d6b70a8292
Nef327 <grigoriirusinkuprash@gmail.com>
2024-09-30 00:39:19 +0400

    add Header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+    <div style="background-color: #01BB72;">
+        <header class="box" ref="header_height">
+            <div>
+                <img class='logo' src="/logo.png" />
+            </div>
+        </header>
+    </div>
+</template>
+<style scoped>  
+header {
+  background-color: #01BB72;
+  top: 0;
+  z-index: 999;
+  width: 100%;
+  position: sticky;
+  padding: 0.4%;
+  padding-left: 2%;
+}
+.logo {
+    width: 135px;
+    height: 72px;
+    
+}
+</style>
