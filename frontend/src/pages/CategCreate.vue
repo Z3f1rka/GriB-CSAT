f5b8f2347d5b11cd020766733bd286e0f0d4f673
Z3f1rka <z.e.f.i.r.k.a88877@gmail.com>
2024-10-09 20:08:06 +0300

    Add CategForm

---
+++ frontend/src/pages/CategCreate.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CategForm from "../components/CategForm.vue";
+</script>
+
+<template>
+  <div class="grid grid-cols-6 grid-rows-1 mt-10">
+    <div class="w-full col-span-4 col-start-2">
+      <CategForm></CategForm>
+    </div>
+  </div>
+</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../pages/Home.vue'
 import Login from '../pages/Login.vue'
 import Card from '../pages/Card.vue'
 import AdminMain from '../pages/AdminMain.vue'
+import CategCreate from '../pages/CategCreate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,7 @@ const router = createRouter({
     { path: '/login', name: 'Login', component: Login },
     { path: '/card', name: 'Card', component: Card },
     { path: '/admin', name: 'AdminPanel', component: AdminMain },
+    { path: '/createcategory', name: 'CreateCategory', component: CategCreate },
   ]
 })
 
