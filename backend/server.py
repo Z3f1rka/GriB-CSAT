c23b70bb623402cf3c0b275c12fc76453be11b34
Alina376 <AlinaK73@yandex.ru>
2024-10-09 19:37:37 +0300

    some more changes

--- backend/server.py
+++ backend/server.py
@@ -21,6 +21,9 @@ from data.products import Product
 from data.feedbacks import Feedback
 from data.characteristics import Characteristic
 
+# random
+from random import sample
+
 app = Flask(__name__)
 access_token_life_time = timedelta(hours=1)
 refresh_token_life_time = timedelta(days=30)
@@ -184,9 +187,7 @@ def products():
     products = sess.query(Product).all()
     res = []
     for product in products:
-        photos = []
-        for photo in product.photos.split(";"):
-            photos.append(photo)
+        photos = product.photos.split(";")
         el = {"id": product.id,
               "title": product.title,
               "img": photos}
@@ -197,29 +198,59 @@ def products():
 
 
 @app.route("/api/product/<int:id>", methods=["GET"])
-def product(id: int):
-    data = request.headers.get("bearer")
+def product(id):
+    """data = request.headers.get("bearer")
     jwt = get_jwt_payload(data)
     if type(jwt) != type(dict()):
-        return make_response(jwt, 401)
-    sess = db_session.create_session()
-    product = sess.query(Product).filter(Product.uuid == id).first()
-    feedbacks = sess.query(Feedback).filter(Feedback.uuid in product.feedback).all()
+        return make_response(jwt, 401)"""
+    sess = db_session.create_session()    
+    product = sess.query(Product).filter(Product.id == id).first()
+    if not product:
+        return make_response('Товар не найден', 404)
+    feedback_id = product.feedback.split(';')
+    feedbacks = []
+    
+    for id in feedback_id:
+        res = sess.query(Feedback).filter(id == Feedback.id).first()
+        feedbacks.append(res)
+        
     for_res = []
     for feedback in feedbacks:
+        print(feedback)
         for_res.append({'name': feedback.user.name,
                         'text': feedback.text,
-                        'img': feedback.photos,
                         'rating': str(feedback.rating),
-                        'date': feedback.public_date,
+                        'data': feedback.public_date,
                         'user_id': feedback.user_id})
-    res = {"ListImg": [],
+    for_res.sort(key=lambda x: len(x['text']), reverse=True)
+    photos = product.photos.split(";")
+    
+    listimg = []
+    for i in range(len(photos)):
+        listimg.append({'id': i + 1, 'img': photos[i]})
+        
+    # user_characteristics = sess.query(Characteristic).all()
+    #random_choice = sample(user_characteristics, 3)
+    # user_characteristic = [i.title for i in random_choice]
+    user_characteristic = []
+    
+    characteristics = sess.query(Product.characteristic_rating).all()
+    characteristics.sort(key=lambda x: x['rating'])
+    print(characteristics)
+    
+    res = {"ListImg": listimg,
            "title": product.title,
+           'rating': product.rating,
+           'maxrating': {'text': '', 'count': 5},
+           'minrating': {'text': '', 'count': 1},
+           'minrating': {},
+           'characteristics': product.characteristics,
            "vendor": product.vendor.name,
-           "characteristic": None,
            "description": product.description,
            "product_type": product.product_type,
-           "feedback": for_res}
+           "feedback": for_res,
+           'user_characteristic': user_characteristic
+           }
     return res
 
 
@@ -248,8 +279,8 @@ def upload_image():
 
 if __name__ == "__main__":
     db_session.global_init('db/CSAT_db.db')
-    db_sess = db_session.create_session()
-    res = db_sess.query(Product).filter(Product.id == 1).first().characteristics
-    res2 = db_sess.query(Characteristic).filter(Characteristic.id == int(res)).first().title
-    print(res2)
+    sess = db_session.create_session()
+    f = sess.query(Product).first()
+    f.feedback = 1
+    sess.commit()
     app.run(port=8080, host="127.0.0.1")
