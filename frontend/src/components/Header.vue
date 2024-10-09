01258ee881531c5772c7f385e6bfe623a542b1a6
SiveruhinMihail <89450.miha@gmail.com>
2024-10-09 17:14:55 +0400

    Edit Header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,12 +3,26 @@
 <template>
   <header class="bg-zinc-900 text-white items-center">
     <div class="flex justify-between">
+      <router-link :to="{ path : '/' }">
       <div class="h-20">
         <img src="/logo.svg" class="w-full h-full" />
       </div>
+      </router-link>
       <div class="grid grid-rows-1 grid-cols-2 items-center pr-20 gap-4 w-1/4">
-        <button class="bg-main h-3/5 w-full rounded"></button>
-        <button class="bg-main h-3/5 w-full rounded"></button>
+        <router-link :to="{ path : '/login' }">
+          <div>
+            <button class="bg-white py-2 w-full rounded text-main text-2xl">
+              Войти
+            </button>
+          </div>
+        </router-link>
+        <router-link :to="{ path : '/register' }">
+          <div>
+          <button class="bg-secondary py-2 w-full rounded text-main text-2xl">
+            Регистрация
+          </button>
+        </div>
+        </router-link>
       </div>
     </div>
   </header>
