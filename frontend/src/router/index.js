9702ce8d7708e4e13918002277acb910c68f1722
Z3f1rka <z.e.f.i.r.k.a88877@gmail.com>
2024-10-09 16:02:16 +0300

    Add categories list

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import Register from '../pages/Register.vue'
 import Home from '../pages/Home.vue'
 import Login from '../pages/Login.vue'
 import Card from '../pages/Card.vue'
+import AdminMain from '../pages/AdminMain.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,7 @@ const router = createRouter({
     { path: '/register', name: 'Register', component: Register },
     { path: '/login', name: 'Login', component: Login },
     { path: '/card', name: 'Card', component: Card },
+    { path: '/admin', name: 'AdminPanel', component: AdminMain },
   ]
 })
 
