8ea938ea75039adceef1ec44561db9d97bcb1d5a
SiveruhinMihail <89450.miha@gmail.com>
2024-10-10 15:08:50 +0400

    Add Error.vue

---
+++ frontend/src/pages/Error.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="grid text-center content-center self-center" style="height: 800px;">
+    <div class="text-9xl pb-5 block border-b-8 border-main" style="margin-left: 10%; margin-right: 10%">Ой что-то пошло не так..</div>
+</div>
+</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../pages/Login.vue'
 import Card from '../pages/Card.vue'
 import AdminMain from '../pages/AdminMain.vue'
 import CategCreate from '../pages/CategCreate.vue'
+import Error from '../pages/Error.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,7 @@ const router = createRouter({
     { path: '/card', name: 'Card', component: Card },
     { path: '/admin', name: 'AdminPanel', component: AdminMain },
     { path: '/createcategory', name: 'CreateCategory', component: CategCreate },
+    { path: '/error', name: 'Error', component: Error },
   ]
 })
 
