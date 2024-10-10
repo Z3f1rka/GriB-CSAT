2c0257f8139863c3bb209fed3581752a85bfb800
SiveruhinMihail <89450.miha@gmail.com>
2024-10-10 15:44:17 +0400

    Edit Header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+
+import { ref } from 'vue';
+
+const isAuth = ref(false)
+if (localStorage.getItem('refresh_token')){
+  isAuth.value = true
+}
+function logout(){
+  localStorage.removeItem('refresh_token');
+}
+</script>
 
 <template>
   <header class="bg-zinc-900 text-white items-center">
@@ -8,7 +19,7 @@
         <img src="/logo.svg" class="w-full h-full" />
       </div>
       </router-link>
-      <div class="grid grid-rows-1 grid-cols-2 items-center pr-20 gap-4 w-1/4">
+      <div v-if="!isAuth" class="grid grid-rows-1 grid-cols-2 items-center pr-20 gap-4 w-1/4">
         <router-link :to="{ path : '/login' }">
           <div>
             <button class="bg-white py-2 w-full rounded text-main text-2xl">
@@ -24,6 +35,21 @@
         </div>
         </router-link>
       </div>
+      <div v-if="isAuth" @click="logout" class="grid self-center mr-16" >
+        <router-link :to="{ path : '/' }">
+          <div class="grid">
+            <div class="bg-white p-2 px-10 w-full rounded text-main text-2xl cursor-pointer cat-button">
+              Выйти
+            </div>
+          </div>
+        </router-link>
+      </div>
     </div>
   </header>
 </template>
+<style scoped>
+.cat-button:hover {
+  background-color: #01bb72;
+  color: white;
+}
+</style>
