e59d49583d40216fa67de5b721dc0a75aa066b88
phantazeeer <shturmovik729@gmail.com>
2024-10-11 05:22:54 +0400

    added tests

---
+++ backend/test/test_card.py
@@ -0,0 +1,51 @@
+import pytest
+import requests
+import os
+from auth import get_access_user
+from test.auth import get_access_vendor
+
+BASEURL = "http://127.0.0.1:8080/api/card/"
+
+def test_add_card():
+    res = requests.post(BASEURL + "all", json={"title": "",
+                                               "description": "",
+                                               "characteristics": "",
+                                               "categories": ""}, headers={"authorization": get_access_vendor()})
+    assert res.status_code == 200
+
+
+def test_add_card_invalid_token():
+    res = requests.post(BASEURL + "all", json={"title": "",
+                                               "description": "",
+                                               "characteristics": "",
+                                               "categories": ""}, headers={"authorization": get_access_user()})
+    assert res.status_code == 403
+
+
+def test_invalid_title():
+    res = requests.post(BASEURL + "all", json={"title": "",
+                                               "description": "",
+                                               "characteristics": "",
+                                               "categories": ""}, headers={"authorization": get_access_vendor()})
+    assert res.status_code == 400
+
+
+def test_not_authorized():
+    res = requests.post(BASEURL + "all", json={"title": "",
+                                               "description": "",
+                                               "characteristics": "",
+                                               "categories": ""})
+    assert res.status_code == 401
+
+def test_product():
+    res = requests.post("http://127.0.0.1:8080/api/product/1", headers={"authorization": get_access_user()})
+    assert res.status_code == 200
+
+
+def test_invalid_token_product():
+    res = requests.post("http://127.0.0.1:8080/api/product/1")
+    assert res.status_code == 401
+
+def test_invalid_product():
+    res = requests.get("http://127.0.0.1:8080/api/product/10", headers={"authorization": get_access_user()})
+    assert res.status_code == 404
---
+++ backend/test/test_feedback.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+import os
+from auth import get_access_user
+from test.auth import get_access_vendor
+
+BASEURL = "http://127.0.0.1:8080/api/feedback/"
+
+def test_feedback_add():
+    res = requests.post(BASEURL + "add", json={"feedback": {"text": "очень классный","product_id": 1},
+                                               "ratings": [{"rating": 4, "criterion": "Целостность"}]})
+    assert res.status_code == 200
