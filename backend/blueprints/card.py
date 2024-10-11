16362328b33f8560ab1be331af1172aeba4febfb
phantazeeer <shturmovik729@gmail.com>
2024-10-11 06:33:21 +0400

    fix product(x2)

--- backend/blueprints/card.py
+++ backend/blueprints/card.py
@@ -44,61 +44,81 @@ def product(id):
     product = sess.query(Product).filter(Product.id == id).first()
     if not product:
         return make_response('Товар не найден', 404)
-    total_med = 0 # конечный результат
-    feedbacks = product.feedbacks # список фидбеков
-    total_sum = 0
-    maxrating = -1
-    minrating = 6
-    for i in feedbacks: # перебор списка фидбеков
-        a = i.ratings # список рейтингов
-        rating_sum = 0 # среднее арифметическое рейтингов
-        for j in a: # перебор рейтингов
-            rating_sum += j.rating
-            if j > maxrating:
-                maxrating = j.rating
-                maxtext = j.title
-            if j < minrating:
-                minrating = j.rating
-                mintext = j.title
-        rating_med = rating_sum / len(a)
-        total_sum += rating_med
-    total_med = total_sum / len(feedbacks)
+    if sess.query(Feedback).filter(Feedback.product_id == product.id).first():
+        total_med = 0 # конечный результат
+        feedbacks = product.feedbacks # список фидбеков
+        total_sum = 0
+        maxrating = -1
+        minrating = 6
+        for i in feedbacks: # перебор списка фидбеков
+            a = i.ratings # список рейтингов
+            rating_sum = 0 # среднее арифметическое рейтингов
+            for j in a: # перебор рейтингов
+                rating_sum += j.rating
+                if j > maxrating:
+                    maxrating = j.rating
+                    maxtext = j.title
+                if j < minrating:
+                    minrating = j.rating
+                    mintext = j.title
+            rating_med = rating_sum / len(a)
+            total_sum += rating_med
+        total_med = total_sum / len(feedbacks)
 
-    rating = total_med
-    
-    for_res = []
-    for feedback in feedbacks:
-        a = feedback.ratings  # список рейтингов
-        rating_sum = 0  # среднее арифметическое рейтингов
-        for j in a:  # перебор рейтингов
-            rating_sum += j.rating
-        rating_med = rating_sum / len(a)
-        for_res.append({'name': feedback.user.name,
-                        'text': feedback.text,
-                        'rating': rating_med,
-                        'data': feedback.public_date,
-                        'user_id': feedback.user_id})
-    for_res.sort(key=lambda x: len(x['text']), reverse=True)
+        rating = total_med
 
-    s = []
-    for j in [i.category for i in product.categories]:
-        for n in j.criterion:
-            s.append(n)
-    
-    random_choice = sample(s, 3)
-    user_characteristic = [i.title for i in random_choice]
+        for_res = []
+        for feedback in feedbacks:
+            a = feedback.ratings  # список рейтингов
+            rating_sum = 0  # среднее арифметическое рейтингов
+            for j in a:  # перебор рейтингов
+                rating_sum += j.rating
+            rating_med = rating_sum / len(a)
+            for_res.append({'name': feedback.user.name,
+                            'text': feedback.text,
+                            'rating': rating_med,
+                            'data': feedback.public_date,
+                            'user_id': feedback.user_id})
+        for_res.sort(key=lambda x: len(x['text']), reverse=True)
 
-    characteristic = product.characteristics
+        s = []
+        for j in [i.category for i in product.categories]:
+            for n in j.criterion:
+                s.append(n)
 
-    res = {"title": product.title,
-           'rating': rating,
-           'maxrating': {'text': maxtext, 'count': maxrating},
-           'minrating': {'text': mintext, 'count': minrating},
-           "vendor": product.vendor.name,
-           "description": product.description,
-           "feedback": for_res,
-           'user_characteristic': user_characteristic,
-           'characteristic': characteristic
-           }
-    
+        random_choice = sample(s, 3)
+        user_characteristic = [i.title for i in random_choice]
+
+        characteristic = product.characteristics
+
+        res = {"title": product.title,
+               'rating': rating,
+               'maxrating': {'text': maxtext, 'count': maxrating},
+               'minrating': {'text': mintext, 'count': minrating},
+               "vendor": product.vendor.name,
+               "description": product.description,
+               "feedback": for_res,
+               'user_characteristic': user_characteristic,
+               'characteristic': characteristic
+               }
+    else:
+        s = []
+        for j in [i.category for i in product.categories]:
+            for n in j.criterion:
+                s.append(n)
+
+        random_choice = sample(s, 3)
+        user_characteristic = [i.title for i in random_choice]
+
+        characteristic = product.characteristics
+        res = {"title": product.title,
+               'rating': 0,
+               'maxrating': {'text': '', 'count': 0},
+               'minrating': {'text': '', 'count': 0},
+               "vendor": product.vendor.name,
+               "description": product.description,
+               "feedback": [],
+               'user_characteristic': user_characteristic,
+               'characteristic': characteristic
+               }
     return res
